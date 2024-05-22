7ee865af71b63a6167795f67ccc3424816d48f65
TAEHONGLEE <2t_hong@TAEHONGLEEui-MacBookPro.local>
2024-05-23 01:53:14 +0900

    style: Adjust logo position in Header component
    Added margin-left to logo in Header component to ensure it is positioned away from the left edge
    Ensured the logo and category buttons are properly aligned and spaced
    Updated CSS for better layout and visual consistency
    This commit improves the layout of the Header component by adjusting the position of the logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CategoryButton from './CategoryButton';
-import './Header.css'; // Header 스타일을 임포트합니다.
+import './Header.css';
 
 const Header = ({ setCategory }) => {
   return (
     <div className="header-container">
       <header className="header">
+        <div className="logo">ClimaX</div>
         <div className="button-container">
           <CategoryButton category="강수량" setCategory={setCategory}>강수량</CategoryButton>
           <CategoryButton category="풍수해" setCategory={setCategory}>풍수해</CategoryButton>
