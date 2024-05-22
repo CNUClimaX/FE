04d8eef040de7a63eeffd06796f05a0497b19e2e
TAEHONGLEE <2t_hong@TAEHONGLEEui-MacBookPro.local>
2024-05-23 01:25:32 +0900

    feat: Update Header component with Category buttons
    Added CategoryButton component to handle category selection with div tags styled as buttons
    Updated Header component to include CategoryButton for category selection
    Adjusted Header component to position buttons to the right
    Added ClimaX logo text to the left side of the Header
    Modified CSS to remove button backgrounds and change text color on hover
    This commit enhances the user interface for category selection and improves the layout of the Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import './Header.css'; // Header 스타일을 임포트합니다.
 
 const Header = ({ setCategory }) => {
   return (
-    <header className="header">
-      <div className="button-container">
-        <CategoryButton category="강수량" setCategory={setCategory}>강수량</CategoryButton>
-        <CategoryButton category="풍수해" setCategory={setCategory}>풍수해</CategoryButton>
-        <CategoryButton category="산사태" setCategory={setCategory}>산사태</CategoryButton>
-      </div>
-    </header>
+    <div className="header-container">
+      <header className="header">
+        <div className="button-container">
+          <CategoryButton category="강수량" setCategory={setCategory}>강수량</CategoryButton>
+          <CategoryButton category="풍수해" setCategory={setCategory}>풍수해</CategoryButton>
+          <CategoryButton category="산사태" setCategory={setCategory}>산사태</CategoryButton>
+        </div>
+      </header>
+    </div>
   );
 };
 
